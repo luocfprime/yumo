dff7d4c40c7e5ebc669afc9ea4cf32b2bb2a7054
Chaofan Luo <luocfprime@gmail.com>
2025-09-13 10:41:23 +0800

    fix: Changed default DENOISE_METHODS to gaussian.

--- yumo/constants.py
+++ yumo/constants.py
@@ -1,3 +1,3 @@
 DATA_PREPROCESS_METHODS = ("identity", "log_e", "log_10")
 CMAPS = ("rainbow", "viridis", "coolwarm", "jet", "turbo", "magma")
-DENOISE_METHODS = ("nearest_and_gaussian", "nearest", "gaussian")
+DENOISE_METHODS = ("gaussian", "nearest_and_gaussian", "nearest")
